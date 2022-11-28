af8ca845a3e815dca30e164d2d55aaff944454b7
Tamires <tamireshc@gmail.com>
2022-11-28 09:04:07 -0300

    ajust auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,11 +2,13 @@ import NextAuth from 'next-auth'
 import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
+  secret: process.env.NEXT_PUBLIC_SECRET,
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
       // scope:'read:user' as any
     })
+
   ]
 })
